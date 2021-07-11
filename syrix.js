00a4828883e0aa482d8ec25cd014c389f66399cf
Syrix <74198394+syrixshu@users.noreply.github.com>
2021-07-11 11:26:37 +0300

    Update syrix.js

--- syrix.js
+++ syrix.js
@@ -1,2 +1,4 @@
+#You will put the code I have given below in the main, but in the const section.
+
 const Constants = require('discord.js/src/util/Constants.js');
 Constants.DefaultOptions.ws.properties.$browser = `Discord Android`
